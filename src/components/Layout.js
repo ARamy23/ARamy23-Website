5acf1b911bff9c6272990ad28a63d85d2c7a981f
Stackbit Code Editor <projects@stackbit.com>
2021-11-26 23:42:40 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -53,7 +53,7 @@ export default class Body extends React.Component {
                     {favicon && <link rel="icon" href={withPrefix(favicon)} />}
                 </Helmet>
                 <div id="site-wrap" className="site">
-                    <div style={{backgroundImage: "linear-gradient(to bottom, #535664, #202D3A)" }}>
+                    <div style={{backgroundImage: "linear-gradient(to right, #535664, #202D3A)" }}>
                         <Header page={page} config={config} />
                     <main id="content" className="site-content">
                         {this.props.children}
