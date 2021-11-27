8fc8cb2c74be7d74060fd16b09e27a2884b63073
Stackbit Code Editor <projects@stackbit.com>
2021-11-27 00:38:47 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -77,7 +77,6 @@ export default class HeroSection extends React.Component {
                                     <Action action={actions[0]} />
                                     <Action action={actions[1]} />
                                 </div>
-
                                 
                             )}
                         </div>
