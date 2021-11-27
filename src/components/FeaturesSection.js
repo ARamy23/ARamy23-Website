49dc98d34120879d4deef49ace71474a3cc5276f
Stackbit Code Editor <projects@stackbit.com>
2021-11-27 01:04:22 +0000

    FeaturesSection.js: changed

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -55,7 +55,7 @@ export default class FeaturesSection extends React.Component {
         return (
             <section className="section section--features">
                 {title && (
-                    <div className="container align-left" >
+                    <div className="container align-left"  style={{backgroundImage: "linear-gradient(to right, #EDF7F8, #B5C7E1)" }} >
                         <div>
                             <h3 className="section__title">{title}</h3>
                         </div>
