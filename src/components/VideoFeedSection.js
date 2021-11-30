d27289fc9fd0986a3e6711aae8807f8e1996de09
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 15:27:41 +0000

    VideoFeedSection.js: changed

--- src/components/VideoFeedSection.js
+++ src/components/VideoFeedSection.js
@@ -2,8 +2,6 @@ import React from 'react';
 import _ from 'lodash';
 
 import {Link, withPrefix, getData, getPageUrl} from '../utils';
-import BlogPostCategories from './VideoCategories';
-import BlogPostMeta from '../components/VideoMeta';
 
 export default class VideoFeedSection extends React.Component {
     renderBlogFeedItemFilter(post, data, section) {
