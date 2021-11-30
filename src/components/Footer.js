b0455bab2e25b9f909a0347594c7efda75d10b01
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 16:37:17 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ export default class Footer extends React.Component {
                     && !_.isEmpty(navLinks)) 
                     || (hasSocial && !_.isEmpty(socialLinks))) && (
                         <div>
-                            <h6>Fid me @</h6>
+                            <h6>Find me @</h6>
                             <div className="site-footer__nav">
                                 {hasNav && !_.isEmpty(navLinks) && (
                                     <ul className="site-footer__menu menu">
